1cfc23057b620a71e33d67e6982fa4270d86377f
Casalg <Casalg@DESKTOP-J00OJ8K>
2020-02-21 23:37:12 -0500

    Inicio

---
+++ src/test/java/edu/cecar/test/Test.java
@@ -0,0 +1,16 @@
+package edu.cecar.test;
+
+/**
+ *
+ * @author Casalg
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
